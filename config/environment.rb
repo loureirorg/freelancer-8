fc705991355c1924e26d359ec51079f065cba86b
Daniel Loureiro <loureirorg@gmail.com>
2015-05-05 17:46:06 -0300

    old fb app

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# ENV['FACEBOOK_KEY'] = "334916903284528"
-# ENV['FACEBOOK_SECRET'] = "a636b51563656666317ab68f34863746"
+ENV['FACEBOOK_KEY'] = "334916903284528"
+ENV['FACEBOOK_SECRET'] = "a636b51563656666317ab68f34863746"
 
-ENV['FACEBOOK_KEY'] = "700728386703376"
-ENV['FACEBOOK_SECRET'] = "549c3b91ada42c091b11300eaf0265d2"
+# ENV['FACEBOOK_KEY'] = "700728386703376"
+# ENV['FACEBOOK_SECRET'] = "549c3b91ada42c091b11300eaf0265d2"
 
 # Initialize the Rails application.
 Rails.application.initialize!
